27b800a5fb41ad2033c4b40b18f35f9fd59ad46d
leo <leolittleprogrammer@gmail.com>
2023-04-13 03:47:57 -0700

    update on the home page

---
+++ Assets/js/index.js
@@ -0,0 +1,11 @@
+let selected = document.querySelector("#selected_D")
+let select_btn = document.querySelectorAll("")
+
+select_btn.addEventListener('click', function(){
+  // if (selected.className == 'selected_D'){
+  //   selected.classList.add('full')
+  // }else{
+  //   selected.className = 'selected_D'
+  // }
+  console.log(select_btn.innerHTML)
+})
--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,18 @@
-let btn_g = document.querySelector(".gonder")
-
-console.log(btn_g)
+// Swiper js 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
